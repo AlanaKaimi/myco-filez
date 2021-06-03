732bcfb9bdc0b8d6326be26ba1425d6150537228
AlanaKaimi <ajbryce@live.com>
2021-06-03 15:55:57 -0400

    add photo's to myco_files resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "myco_files#index"
   
-  resources :myco_files, path: '/myco-filez'
+  resources :myco_files, path: '/myco-filez' do
+    resources :photos
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
