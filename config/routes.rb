c464a015bc7292a8a85ab5ed04817d5ce534f8cc
AlanaKaimi <ajbryce@live.com>
2021-06-03 11:19:33 -0400

    add routes to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "/myco-filez", to: "mycofilez#index"
+  root "myco_files#index"
+  
+  get "/myco-filez", to: "myco_files#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
