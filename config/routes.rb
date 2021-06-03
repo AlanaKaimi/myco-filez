d31c202a12aede1e9b472c1758a9231acddba1b5
AlanaKaimi <ajbryce@live.com>
2021-06-03 12:54:41 -0400

    Polish up myco_file routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "myco_files#index"
   
-  get "/myco-filez", to: "myco_files#index"
-  get "/myco-filez/:id", to: "myco_files#show"
+  resources :myco_files, path: '/myco-filez'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
