459a429abff507c4e1453617991129ca53a61088
AlanaKaimi <ajbryce@live.com>
2021-06-03 12:37:58 -0400

    Create & show single myco_file

--- app/controllers/myco_files_controller.rb
+++ app/controllers/myco_files_controller.rb
@@ -2,4 +2,8 @@ class MycoFilesController < ApplicationController
   def index
     @myco_files = MycoFile.all
   end
+
+  def show
+    @myco_file = MycoFile.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "myco_files#index"
   
   get "/myco-filez", to: "myco_files#index"
+  get "/myco-filez/:id", to: "myco_files#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
