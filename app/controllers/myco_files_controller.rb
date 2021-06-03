a30fb2a66a3b306bd01cb0e20f6450c7c09fdb82
AlanaKaimi <ajbryce@live.com>
2021-06-03 13:01:14 -0400

    add new and create actions to MycoFiles controller

--- app/controllers/myco_files_controller.rb
+++ app/controllers/myco_files_controller.rb
@@ -6,4 +6,19 @@ class MycoFilesController < ApplicationController
   def show
     @myco_file = MycoFile.find(params[:id])
   end
+
+  def new
+    @myco_file = MycoFile.new
+  end
+
+  def create
+    @myco_file = MycoFile.new(name: "...", shape: "...", color: "...", gills: 0, pores: 0, edibility: "...", season: "...", location: "...")
+
+    if @myco_file.save
+      redirect_to @myco_file
+    else
+      render :new
+    end
+  end
+
 end
